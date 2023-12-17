ca51e16623b1df02327e1b908a2e2a150832aceb
Godbell <12191579@inha.edu>
2023-12-18 02:40:58 +0900

    Feature: establish PostgreSQL, MongoDB, Redis database connection (#1)
    * Feat: establish PostgreSQL database connection
    
    * Feat: establish MongoDB database connection
    
    * Feat: establish Redis database connection
    
    * Feat: separate dev and production db
    
    * Style: separate down exported functions in database modules

---
+++ router/test.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import asyncify from 'express-asyncify'
+import { pgQuery } from '@database/postgres'
+import { configDotenv } from 'dotenv'
+import { useMongoModel } from '../database/mongodb'
+import { testSchema } from '../models/test'
+import { redisClient } from '@database/redis'
+import { getFromRedis, setToRedis } from '../database/redis'
+
+configDotenv()
+
+export const testRouter = asyncify(express.Router())
+
+testRouter.get('/postgres/connection', async (req, res) => {
+  console.log(
+    `sending query to ... ${process.env.RDB_HOST}:${process.env.RDB_PORT}`,
+  )
+
+  const result = await pgQuery(`SELECT * FROM kkujjang_test.test;`)
+
+  res.send(`connection successful, test result: ${JSON.stringify(result)}`)
+})
+
+testRouter.get('/mongodb/connection', async (req, res) => {
+  console.log(
+    `sending query to ... ${process.env.DDB_HOST}:${process.env.DDB_PORT}`,
+  )
+
+  const result = await useMongoModel('test', testSchema).find({})
+
+  res.send(`connection successful, test result: ${JSON.stringify(result)}`)
+})
+
+testRouter.get('/redis/connection', async (req, res) => {
+  console.log(
+    `getting data from ... ${process.env.CACHE_HOST}:${process.env.CACHE_PORT}`,
+  )
+
+  await setToRedis('test', 'connection successful')
+  const result = await getFromRedis('test')
+
+  res.send(`connection successful, test result: ${result}`)
+})
