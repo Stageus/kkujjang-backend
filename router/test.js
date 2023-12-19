4d518666071c71e274043e8967c6f3466df2a7e8
GulSauce <pine7420@naver.com>
2023-12-19 22:23:59 +0900

    Feat: add validation test -/user 경로로 유효성 검사 테스트 할수 있는 라우터 추가

--- router/test.js
+++ router/test.js
@@ -6,6 +6,7 @@ import { useMongoModel } from '../database/mongodb'
 import { testSchema } from '../models/test'
 import { redisClient } from '@database/redis'
 import { getFromRedis, setToRedis } from '../database/redis'
+import * as v from '../utility/validation'
 
 configDotenv()
 
@@ -40,4 +41,38 @@ testRouter.get('/redis/connection', async (req, res) => {
   const result = await getFromRedis('test')
 
   res.send(`connection successful, test result: ${result}`)
+})
+
+testRouter.post('/user', async (req, res) => {
+  const { username, password, nickname, phone, authId } = req.body
+  v.check(
+    username,
+    `username checking`,
+    v.checkExist(),
+    v.checkLength(7, 30),
+    v.checkRegExp(/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/),
+  )
+  v.check(
+    password,
+    `password checking`,
+    v.checkExist(),
+    v.checkLength(7, 30),
+    v.checkRegExp(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[@#$%^&+=!\(\)])[a-zA-Z0-9@#$%^&+=!\(\)]+$/,
+    ),
+  )
+  v.check(
+    nickname,
+    `nickname checking`,
+    v.checkExist(),
+    v.checkLength(1, 15),
+    v.checkRegExp(/^[a-zA-Z0-9가-힣]+$/),
+  )
+  v.check(
+    phone,
+    `phone checking`,
+    v.checkExist(),
+    v.checkRegExp(/^010-\d{4}-\d{4}$/),
+  )
+  res.send(`validation check successful`)
 })
