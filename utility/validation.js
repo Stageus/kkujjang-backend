28f1f6ff7e1eee8d9b24db684f3c0ae09087ba8c
GulSauce <pine7420@naver.com>
2023-12-19 23:38:23 +0900

    Style: add enter between line 11 and line12 change return string

--- utility/validation.js
+++ utility/validation.js
@@ -9,6 +9,7 @@ const curry =
     }
     return (...appendValues) => curry(func)(...inputValues, ...appendValues)
   }
+
 // combine 전달받은 변수와 함수를 가공하여
 // 하나의 값을 return 또는 throw해 줍니다.
 // 결국 iter를 하나의 값으로 압축했기 때문에 combine 함수입니다.
@@ -16,9 +17,9 @@ const combine = (iter) => {
   const funcs = iter[Symbol.iterator]()
 
   const target = funcs.next().value
-  const msg = funcs.next().value
+  const targetType = funcs.next().value
 
-  const reason = Array.from(funcs).reduce((errors, func) => {
+  const messages = Array.from(funcs).reduce((errors, func) => {
     const result = func(target)
     result !== true && errors.push(result)
     return errors
@@ -27,8 +28,8 @@ const combine = (iter) => {
   if (reason.length != 0) {
     throw {
       statusCode: 400,
-      where: msg,
-      reason: reason,
+      targetString: targetType,
+      messages,
     }
   }
 }
@@ -37,14 +38,19 @@ export const checkExist = curry((target) => {
   if (target != '' && target != null && target != undefined) {
     return true
   }
-  return `checkExist에서 실패했습니다.`
+  return `checkExist: 값이 존재하지 않습니다.`
 })
 
 export const checkLength = curry((min, max, target) => {
   if (min <= target.length && target.length <= max) {
     return true
   }
-  return `checkLength에서 실패했습니다.`
+  if (target.length < min) {
+    return `checkLength: 길이가 너무 짧습니다.`
+  }
+  if (max < target.length) {
+    return `checkLength: 길이가 너무 깁니다.`
+  }
 })
 
 export const checkRegExp = curry((std, target) => {
