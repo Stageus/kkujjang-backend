c9f011749c7dfee94388fa98e4979f30a388283f
GulSauce <pine7420@naver.com>
2023-12-19 20:28:54 +0900

    Fix: alter variable names more clearly

--- utility/validation.js
+++ utility/validation.js
@@ -1,16 +1,18 @@
-import { json } from 'express'
-
-// 주석의 내용
+// curry는 inputValues의 개수에 따라
+// 다시 한번 inputValues를 요구하는 함수를 return해줄지
+// 곧바로 inputValues를 func에 전달하여 실행해줄지 결정해줍니다.
 const curry =
-  (f) =>
-  (...as) => {
-    if (f.length <= as.length) {
-      return f(...as)
+  (func) =>
+  (...inputValues) => {
+    if (func.length <= inputValues.length) {
+      return func(...inputValues)
     }
-    return (...bs) => curry(f)(...as, ...bs)
+    return (...appendValues) => curry(func)(...inputValues, ...appendValues)
   }
-// 주석의 내용
-const combine = (f, iter) => {
+// combine 전달받은 변수와 함수를 가공하여
+// 하나의 값을 return 또는 throw해 줍니다.
+// 결국 iter를 하나의 값으로 압축했기 때문에 combine 함수입니다.
+const combine = (iter) => {
   const funcs = iter[Symbol.iterator]()
 
   const target = funcs.next().value
@@ -25,7 +27,7 @@ const combine = (f, iter) => {
   if (reason.length != 0) {
     throw {
       statusCode: 400,
-      where: msg + ` 유효성 검사 중`,
+      where: msg,
       reason: reason,
     }
   }
@@ -35,28 +37,28 @@ export const checkExist = curry((target) => {
   if (target != '' && target != null && target != undefined) {
     return true
   }
-  return `checkExist에서 유효성 검사가 실패했습니다.`
+  return `checkExist에서 실패했습니다.`
 })
 
 export const checkLength = curry((min, max, target) => {
   if (min <= target.length && target.length <= max) {
     return true
   }
-  return `checkLength에서 유효성 검사가 실패했습니다.`
+  return `checkLength에서 실패했습니다.`
 })
 
 export const checkRegExp = curry((std, target) => {
   if (RegExp(std).test(target)) {
     return true
   }
-  return `checkRegExp에서 유효성 검사가 실패했습니다.`
+  return `checkRegExp에서 실패했습니다.`
 })
 
 export const checkSame = curry((sameTarget, target) => {
   if (target == sameTarget) {
     return true
   }
-  return `checkSame에서 유효성 검사가 실패했습니다.`
+  return `checkSame에서 실패했습니다.`
 })
 
-export const check = (...as) => combine((as, f) => f(as), as)
+export const check = (...args) => combine(args)
