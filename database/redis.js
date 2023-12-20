8a26b7d4f681ec4b234caaf226aaba502439d465
Godbell <12191579@inha.edu>
2023-12-20 14:27:56 +0900

    Refactor: provide only redis client object rather than creating all of redis function callers

--- database/redis.js
+++ database/redis.js
@@ -13,22 +13,11 @@ const dbConfig = {
       : process.env.CACHE_TEST_DB,
 }
 
-const redisClient = redis.createClient({
+export const redisClient = redis.createClient({
   url: `redis://:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`,
 })
-
 ;(async () => {
   await redisClient.connect()
 })()
 
 console.log(`Created Redis Connection to ${dbConfig.host}:${dbConfig.port}`)
-
-export const getFromRedis = async (key) => {
-  const result = await redisClient.get(key)
-
-  return result
-}
-
-export const setToRedis = async (key, value) => {
-  await redisClient.set(key, value)
-}
