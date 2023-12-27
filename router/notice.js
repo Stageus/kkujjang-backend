6c5188ff25f3ffec97cf9673e65bf2f3c4a0651e
Godbell <12191579@inha.edu>
2023-12-28 01:49:50 +0900

    Feat: create GET /notice/search

--- router/notice.js
+++ router/notice.js
@@ -2,7 +2,7 @@ import express from 'express'
 import asyncify from 'express-asyncify'
 import { getSession } from '@utility/session'
 import { configDotenv } from 'dotenv'
-import * as validtion from '@utility/validation'
+import * as validation from '@utility/validation'
 import { pgQuery } from '@database/postgres'
 
 configDotenv()
@@ -26,8 +26,8 @@ noticeRouter.post('/', async (req, res) => {
   }
 
   const { title, content } = req.body
-  validtion.check(title, 'title', validtion.checkExist())
-  validtion.check(content, 'content', validtion.checkExist())
+  validation.check(title, 'title', validation.checkExist())
+  validation.check(content, 'content', validation.checkExist())
 
   await pgQuery(
     `INSERT INTO kkujjang.notice (title, content, author_id) VALUES ($1, $2, $3);`,
@@ -53,9 +53,34 @@ noticeRouter.get('/list', async (req, res) => {
   res.json({ result })
 })
 
+noticeRouter.get('/search', async (req, res) => {
+  const page = Number(req.query.page ?? 1)
+  const keyword = req.query.q
+
+  validation.check(
+    validation,
+    'keyword',
+    validation.checkExist(),
+    validation.checkRegExp(/[a-zA-Z가-힣0-9 ].+/),
+  )
+
+  const result = (
+    await pgQuery(
+      `SELECT title, content, created_at, views 
+      FROM kkujjang.notice 
+      WHERE is_deleted=FALSE
+      AND title LIKE '%${keyword}%'
+      ORDER BY created_at DESC
+      OFFSET ${(page - 1) * 10} LIMIT 10`,
+    )
+  ).rows
+
+  res.json({ result })
+})
+
 noticeRouter.get('/:noticeId', async (req, res) => {
   const { noticeId } = req.params
-  validtion.check(noticeId, 'noticeId', validtion.checkExist())
+  validation.check(noticeId, 'noticeId', validation.checkExist())
 
   const result = (
     await pgQuery(
@@ -90,11 +115,11 @@ noticeRouter.put('/:noticeId', async (req, res) => {
   }
 
   const { noticeId } = req.params
-  validtion.check(noticeId, 'noticeId', validtion.checkExist())
+  validation.check(noticeId, 'noticeId', validation.checkExist())
 
   const { title, content } = req.body
-  validtion.check(title, 'title', validtion.checkExist())
-  validtion.check(content, 'content', validtion.checkExist())
+  validation.check(title, 'title', validation.checkExist())
+  validation.check(content, 'content', validation.checkExist())
 
   await pgQuery(
     `UPDATE kkujjang.notice SET title=$1, content=$2 
@@ -119,11 +144,11 @@ noticeRouter.delete('/:noticeId', async (req, res) => {
   }
 
   const { noticeId } = req.params
-  validtion.check(noticeId, 'noticeId', validtion.checkExist())
+  validation.check(noticeId, 'noticeId', validation.checkExist())
 
   const { title, content } = req.body
-  validtion.check(title, 'title', validtion.checkExist())
-  validtion.check(content, 'content', validtion.checkExist())
+  validation.check(title, 'title', validation.checkExist())
+  validation.check(content, 'content', validation.checkExist())
 
   await pgQuery(
     `UPDATE kkujjang.notice SET is_deleted=TRUE WHERE id=$1 AND is_deleted=FALSE`,
