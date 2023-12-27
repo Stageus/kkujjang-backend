dfc4422e28bc320630c429b0014f32c6d970895e
GulSauce <pine7420@naver.com>
2023-12-27 12:58:49 +0900

    Feat: check multi-login using function isSignedIn()

--- router/user.js
+++ router/user.js
@@ -7,6 +7,7 @@ import * as kakao from '@utility/kakao'
 import { getSession, createSession, destorySession } from '@utility/session'
 import * as uuid from 'uuid'
 import * as validation from '@utility/validation'
+import { isSignedIn } from '@utility/session'
 
 configDotenv()
 
@@ -177,26 +178,33 @@ userRouter.post('/signin', async (req, res) => {
   }
 
   const { username, password } = req.body
-  const queryString = `SELECT kkujjang.user.id, authority_level FROM kkujjang.user
+  const queryString = `SELECT id, authority_level FROM kkujjang.user
   WHERE username = $1 AND password = crypt($2, password)`
   const values = [username, password]
   const queryRes = await pgQuery(queryString, values)
 
   if (queryRes.rowCount == 0) {
-    res.send({
-      result: 'fail',
+    throw {
+      statusCode: 401,
       message: '존재하지 않는 계정 정보입니다.',
-    })
+    }
   }
 
   if (2 <= queryRes.rowCount) {
     throw {
       statusCode: 500,
-      message: '여러개의 동일한 계정이 user_auth 테이블에 존재합니다',
+      message: '여러개의 동일한 계정이 user_auth 테이블에 존재합니다.',
     }
   }
 
   const { id: userId, authority_level: authorityLevel } = queryRes.rows[0]
+
+  if (await isSignedIn(userId)) {
+    throw {
+      statusCode: 400,
+      message: '접속중인 계정입니다.',
+    }
+  }
 
   const sessionId = await createSession({
     userId,
@@ -214,19 +222,84 @@ userRouter.post('/signin', async (req, res) => {
   })
 })
 
-// 유저 검색
+// 특정 조건에 맞는 사용자 검색
 userRouter.get('/search', async (req, res) => {
-  const username = req.params.username
-  throw username
-  const nickname = req.params.nickname
-  const isBanned = req.params.isBanned
+  // Permission : 관리자
+  const sessionId = req.cookies.sessionId
+  if (!sessionId) {
+    throw {
+      statusCode: 401,
+      message: '로그인 해주세요',
+    }
+  }
+  const authorityLevel = (await getSession(sessionId)).authorityLevel
+  if (authorityLevel != 1) {
+    throw {
+      statusCode: 401,
+      message: '관리자 권한이 없습니다.',
+    }
+  }
+  const username = req.query.username
+  const nickname = req.query.nickname
+  const isBanned = req.query.isBanned
+
+  const conditions = []
+  const values = []
+  let conditionCnt = 1
+  if (username) {
+    conditions.push(`username = $${conditionCnt++}`)
+    values.push(username)
+  }
+  if (nickname) {
+    conditions.push(`nickname = $${conditionCnt++}`)
+    values.push(nickname)
+  }
+  if (isBanned) {
+    conditions.push(`is_banned = $${conditionCnt++}`)
+    values.push(isBanned)
+  }
+
+  let queryString = `SELECT id, username, nickname, is_banned FROM kkujjang.user WHERE is_deleted = FALSE`
+
+  if (conditions.length) {
+    queryString += ' AND ' + conditions.join(' AND ')
+  }
+
+  const queryRes = await pgQuery(queryString, values)
+  const result = []
+  for (const data of queryRes.rows) {
+    console.log(data)
+    const { id, username, nickname, is_banned } = data
+    const temp = { id, username, nickname, isBanned: is_banned }
+    result.push(temp)
+  }
+  res.json({
+    result,
+  })
 })
+
+// 비밀번호 찾기
+userRouter.post('/find/pw', async (req, res) => {})
+
+// 아이디 찾기
+userRouter.post('/find/pw', async (req, res) => {})
 
 // 문자 인증정보 생성(임시)
 userRouter.get('/tempAuth-code', async (req, res) => {
   // Permission : 누구나
   const { phone } = req.body
   const authId = uuid.v4()
+
+  let queryString = `SELECT count(*) FROM  kkujjang.user WHERE phone = $1`
+  let values = [phone]
+  let queryRes = await pgQuery(queryString, values)
+
+  if (0 < queryRes.rows[0].count) {
+    throw {
+      statusCode: 400,
+      message: '중복된 휴대폰 번호가 존재합니다.',
+    }
+  }
 
   await redisClient.hSet(`auth-${authId}`, {
     authNumber: phone,
@@ -239,6 +312,111 @@ userRouter.get('/tempAuth-code', async (req, res) => {
     'Set-Cookie',
     `smsAuthId=${authId}; HttpOnly; Path=/; Secure; Max-Age=3600`,
   )
+
+  res.json({
+    result: 'success',
+  })
+})
+
+// 인덱스를 이용한 사용자 검색
+userRouter.get('/:id', async (req, res) => {
+  // Permission : 사용자, 관리자
+  const sessionId = req.cookies.sessionId
+  if (!sessionId) {
+    throw {
+      statusCode: 401,
+      message: '로그인하지 않은 상태입니다.',
+    }
+  }
+
+  const id = req.params.id
+  const queryString = `SELECT level, nickname, wins, loses, is_banned, banned_reason FROM kkujjang.user WHERE id = $1 AND is_deleted = FALSE`
+  const values = [id]
+  const queryRes = await pgQuery(queryString, values)
+
+  if (queryRes.rowCount == 0) {
+    throw {
+      statusCode: 400,
+      message: '존재하지 않는 id입니다.',
+    }
+  }
+  if (2 <= queryRes.rowCount) {
+    throw {
+      statusCode: 500,
+      message: '동일한 id가 2개 이상 존재합니다.',
+    }
+  }
+
+  const { level, nickname, wins, loses, is_banned, banned_reason } =
+    queryRes.rows[0]
+
+  let winRate
+  if (wins == 0 && loses == 0) {
+    winRate = 0.0
+  } else if (loses == 0) {
+    winRate = 100.0
+  } else {
+    winRate = (wins / (wins + loses)) * 100
+    winRate = winRate.toFixed(1)
+    winRate = Number(winRate)
+  }
+  let result = {
+    level,
+    nickname,
+    winRate,
+  }
+
+  const authorityLevel = (await getSession(sessionId)).authorityLevel
+  if (authorityLevel == 1) {
+    result.isBanned = is_banned
+    result.bannedReason = banned_reason
+  }
+  res.send(result)
+})
+
+// 회원 탈퇴
+userRouter.delete('/', async (req, res) => {
+  // Permission : 사용자, 관리자
+  const sessionId = req.cookies.sessionId
+  if (!sessionId) {
+    throw {
+      statusCode: 401,
+      message: '로그인하지 않은 상태입니다.',
+    }
+  }
+
+  const id = (await getSession(sessionId)).userId
+  const queryString = `UPDATE kkujjang.user SET kakao_id = null, username = NULL, phone = NULL, is_deleted = TRUE WHERE id = $1`
+  const values = [id]
+  await pgQuery(queryString, values)
+
+  res.setHeader(
+    'Set-Cookie',
+    `sessionId=none; Path=/; HttpOnly; Secure; Max-Age=0`,
+  )
+  await destorySession(sessionId)
+
+  res.json({
+    result: 'success',
+  })
+})
+
+// 회원 정보 수정
+userRouter.put('/', async (req, res) => {
+  // Permission : 사용자, 관리자
+  const sessionId = req.cookies.sessionId
+  if (!sessionId) {
+    throw {
+      statusCode: 401,
+      message: '로그인하지 않은 상태입니다.',
+    }
+  }
+
+  const { nickname } = req.body
+  const id = (await getSession(sessionId)).userId
+  const queryString = `UPDATE kkujjang.user SET nickname = $1 WHERE id = $2`
+  const values = [nickname, id]
+  await pgQuery(queryString, values)
 
   res.json({
     result: 'success',
@@ -304,6 +482,7 @@ userRouter.post('/', async (req, res) => {
   let queryString = `SELECT count(*) FROM  kkujjang.user WHERE username = $1 OR phone = $2`
   let values = [username, phone]
   let queryRes = await pgQuery(queryString, values)
+
   if (0 < queryRes.rows[0].count) {
     throw {
       statusCode: 400,
@@ -313,111 +492,6 @@ userRouter.post('/', async (req, res) => {
 
   queryString = `INSERT INTO kkujjang.user (username, password, phone) VALUES ($1, crypt($2, gen_salt('bf')), $3)`
   values = [username, password, phone]
-  await pgQuery(queryString, values)
-
-  res.json({
-    result: 'success',
-  })
-})
-
-// 인덱스를 이용한 사용자 검색
-userRouter.get('/:id', async (req, res) => {
-  // Permission : 사용자, 관리자
-  const sessionId = req.cookies.sessionId
-  if (!sessionId) {
-    throw {
-      statusCode: 401,
-      message: '로그인하지 않은 상태입니다.',
-    }
-  }
-
-  const id = req.params.id
-  const queryString = `SELECT level, nickname, wins, loses, is_banned, banned_reason FROM kkujjang.user WHERE id = $1 AND is_deleted = false`
-  const values = [id]
-  const queryRes = await pgQuery(queryString, values)
-
-  if (queryRes.rowCount == 0) {
-    throw {
-      statusCode: 400,
-      message: '존재하지 않는 id입니다.',
-    }
-  }
-  if (2 <= queryRes.rowCount) {
-    throw {
-      statusCode: 500,
-      message: '동일한 id가 2개 이상 존재합니다.',
-    }
-  }
-
-  const { level, nickname, wins, loses, is_banned, banned_reason } =
-    queryRes.rows[0]
-
-  let winRate
-  if (wins == 0 && loses == 0) {
-    winRate = 0.0
-  } else if (loses == 0) {
-    winRate = 100.0
-  } else {
-    winRate = (wins / (wins + loses)) * 100
-    winRate = winRate.toFixed(1)
-    winRate = Number(winRate)
-  }
-  let result = {
-    level,
-    nickname,
-    winRate,
-  }
-
-  const authorityLevel = (await getSession(sessionId)).authorityLevel
-  if (authorityLevel == 1) {
-    result.isBanned = is_banned
-    result.bannedReason = banned_reason
-  }
-  res.send(result)
-})
-
-// 회원 탈퇴
-userRouter.delete('/', async (req, res) => {
-  // Permission : 사용자, 관리자
-  const sessionId = req.cookies.sessionId
-  if (!sessionId) {
-    throw {
-      statusCode: 401,
-      message: '로그인하지 않은 상태입니다.',
-    }
-  }
-
-  const id = (await getSession(sessionId)).userId
-  const queryString = `UPDATE kkujjang.user SET is_deleted = true WHERE id = $1`
-  const values = [id]
-  await pgQuery(queryString, values)
-
-  res.setHeader(
-    'Set-Cookie',
-    `sessionId=none; Path=/; HttpOnly; Secure; Max-Age=0`,
-  )
-  await destorySession(sessionId)
-
-  res.json({
-    result: 'success',
-  })
-})
-
-// 회원 정보 수정
-userRouter.post('/', async (req, res) => {
-  // Permission : 사용자, 관리자
-  const sessionId = req.cookies.sessionId
-  if (!sessionId) {
-    throw {
-      statusCode: 401,
-      message: '로그인하지 않은 상태입니다.',
-    }
-  }
-
-  const { nickname } = req.body
-  const id = (await getSession(sessionId)).userId
-  const queryString = `UPDATE kkujjang.user SET nickname = $1 WHERE id = $2`
-  const values = [nickname, id]
   await pgQuery(queryString, values)
 
   res.json({
