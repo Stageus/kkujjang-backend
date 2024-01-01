a972b86dcae6e8fa9fc0b9a8b1cd6ee3b47a74c4
GulSauce <pine7420@naver.com>
2024-01-02 08:29:20 +0900

    Fix: erase annotation

--- utility/fileAnalyzer.js
+++ utility/fileAnalyzer.js
@@ -125,21 +125,21 @@ export const fileAnalyzer = (req, limits, options) =>
         fileStream.once('data', function (firstChunk) {
           const type = checkMagicNumber(firstChunk)
           // 불일치한다면 해당 파일 stream을 보내지 않는다
-          // if (`.${type.ext}` !== path.extname(filename)) {
-          //   errResult.push({
-          //     valid: false,
-          //     message: `fileAnalyzer: ${filename} | 알 수 확장자 또는 확장자가 변조된 파일입니다`,
-          //   })
-          //   return fileStream.resume()
-          // }
+          if (`.${type.ext}` !== path.extname(filename)) {
+            errResult.push({
+              valid: false,
+              message: `fileAnalyzer: ${filename} | 알 수 확장자 또는 확장자가 변조된 파일입니다`,
+            })
+            return fileStream.resume()
+          }
 
-          // if (!allowedExtension.includes(type.ext)) {
-          //   errResult.push({
-          //     valid: false,
-          //     message: `fileAnalyzer: ${filename} | 허용되지 않은 확장자입니다`,
-          //   })
-          //   return fileStream.resume()
-          // }
+          if (!allowedExtension.includes(type.ext)) {
+            errResult.push({
+              valid: false,
+              message: `fileAnalyzer: ${filename} | 허용되지 않은 확장자입니다`,
+            })
+            return fileStream.resume()
+          }
 
           // 일치한다면 pipeline 구성 작업
           passThrough.write(firstChunk)
