59b946cf68b9b3f57a2ae95acd7c1b880739cce7
GulSauce <pine7420@naver.com>
2024-01-11 15:41:51 +0900

    Feat: inquiry router

--- middleware/inquiry.js
+++ middleware/inquiry.js
@@ -1,8 +1,11 @@
 import * as validation from '@utility/validation'
 import { multer } from '@utility/kkujjang_multer/core'
 
-export const validateInquirySearch = (req, res, next) => {
+export const validateInquiryGetBySearch = (req, res, next) => {
   const { needAnswer, type } = req.query
+
+  const conditionStrArray = []
+  const conditionValue = []
 
   if (needAnswer !== null && needAnswer !== undefined) {
     validation.check(
@@ -11,6 +14,8 @@ export const validateInquirySearch = (req, res, next) => {
       validation.checkExist(),
       validation.checkRegExp(/^(true|false)$/),
     )
+    conditionValue.push(needAnswer)
+    conditionStrArray.push(`AND need_answer = $${conditionValue.length}`)
   }
 
   if (type !== null && type !== undefined) {
@@ -20,14 +25,31 @@ export const validateInquirySearch = (req, res, next) => {
       validation.checkExist(),
       validation.checkIsNumber(),
     )
+    conditionValue.push(type)
+    conditionStrArray.push(`AND type = $${conditionValue.length}`)
   }
+
+  res.locals.conditionString = conditionStrArray.join(' ')
+  res.locals.conditionValue = conditionValue
 
   next()
 }
 
-export const validateInquiryPathIndex = (req, res, next) => {
+export const validateInquiryGetByList = (req, res, next) => {
+  const session = res.locals.session
+  if (session.authorityLevel === process.env.ADMIN_AUTHORITY) {
+    next()
+    return
+  }
+
+  res.locals.conditionString = 'WHERE author_id = $1'
+  res.locals.conditionValue = [session.userId]
+
+  next()
+}
+
+export const validateInquiryGetByPathIndex = (req, res, next) => {
   const { inquiryId } = req.params
-  console.log(inquiryId)
 
   validation.check(
     inquiryId,
@@ -36,12 +58,86 @@ export const validateInquiryPathIndex = (req, res, next) => {
     validation.checkIsNumber(),
   )
 
+  const session = res.locals.session
+
+  if (Number(session.authorityLevel) === Number(process.env.ADMIN_AUTHORITY)) {
+    next()
+    return
+  }
+
+  res.locals.conditionString = 'AND author_id = $3'
+  res.locals.condtionValue = [session.userId]
+
+  next()
+}
+
+export const validateInquiryPost = (req, res, next) => {
+  const { title, content, type } = req.body
+
+  validation.check(
+    title,
+    'title',
+    validation.checkExist(),
+    validation.checkLength(1, 100),
+  )
+  validation.check(
+    content,
+    'content',
+    validation.checkExist(),
+    validation.checkLength(1, 2000),
+  )
+  validation.check(
+    type,
+    'type',
+    validation.checkExist(),
+    validation.checkRegExp(/^[0-9]$/),
+  )
+
+  const session = res.locals.session
+
+  res.locals.needAnswer =
+    session.authorityLevel !== process.env.ADMIN_AUTHORITY ? true : false
+
+  if (req.files.length === 0) {
+    next()
+    return
+  }
+
+  const fileOrderSubArray = req.files.reduce((acc, file, index) => {
+    acc.push(`$${index * 2 + 7}`)
+    return acc
+  }, [])
+  const keySubarray = req.files.reduce((acc, file, index) => {
+    acc.push(`$${index * 2 + 1 + 7}`)
+    return acc
+  }, [])
+  const fileOrderArrayStirng = `unnest(ARRAY[${fileOrderSubArray.join(', ')}])`
+  const keyArrayString = `unnest(ARRAY[${keySubarray.join(', ')}])`
+
+  const valuesToInsertQuery = `,
+  valuesToInsert AS (
+    SELECT
+      (SELECT id FROM inserted_article) AS inquiry_id,
+      ${fileOrderArrayStirng} AS file_order,
+      ${keyArrayString} AS key
+  )`
+  const fileInsertQuery = `${valuesToInsertQuery} INSERT INTO kkujjang.inquiry_file (inquiry_id, file_order, key)
+  SELECT inquiry_id, CAST(file_order AS INTEGER), key
+  FROM valuesToInsert`
+
+  const fileValue = req.files.reduce((acc, file, index) => {
+    acc.push(Number(index + 1), file)
+    return acc
+  }, [])
+
+  res.locals.fileInsertQuery = fileInsertQuery
+  res.locals.fileValue = fileValue
+
   next()
 }
 
 export const validateInquiryupload = async (req, res, next) => {
   const session = res.locals.session
-
   let author = null
 
   if (Number(session.authorityLevel) !== Number(process.env.ADMIN_AUTHORITY)) {
@@ -66,30 +162,6 @@ export const validateInquiryupload = async (req, res, next) => {
   const parseReuslt = await multer(req, config, options)
   req.body = parseReuslt.text
   req.files = parseReuslt.files
-
-  next()
-}
-
-export const validateInquiry = (req, res, next) => {
-  const { title, content, type } = req.body
-  validation.check(
-    title,
-    'title',
-    validation.checkExist(),
-    validation.checkLength(1, 100),
-  )
-  validation.check(
-    content,
-    'content',
-    validation.checkExist(),
-    validation.checkLength(1, 2000),
-  )
-  validation.check(
-    type,
-    'type',
-    validation.checkExist(),
-    validation.checkRegExp(/^[0-9]$/),
-  )
 
   next()
 }
--- router/inquiry.js
+++ router/inquiry.js
@@ -4,10 +4,11 @@ import { configDotenv } from 'dotenv'
 import { pgQuery } from '@database/postgres'
 import { requireSignin, requireAdminAuthority } from '@middleware/auth'
 import {
-  validateInquirySearch,
-  validateInquiryPathIndex,
+  validateInquiryGetBySearch,
+  validateInquiryGetByList,
+  validateInquiryGetByPathIndex,
+  validateInquiryPost,
   validateInquiryupload,
-  validateInquiry,
 } from '@middleware/inquiry'
 import { validatePageNumber } from '@middleware/page'
 
@@ -19,53 +20,35 @@ export const inquiryRouter = asyncify(express.Router())
 inquiryRouter.get(
   '/search',
   requireAdminAuthority,
-  validateInquirySearch,
   validatePageNumber,
+  validateInquiryGetBySearch,
   async (req, res) => {
-    const { page, needAnswer = null, type = null } = req.query
-    const values = []
-    let conditionArray = []
-
-    if (needAnswer !== null) {
-      values.push(needAnswer)
-      conditionArray.push(`AND inq_na.need_answer = $${values.length}`)
-    }
-
-    if (type !== null) {
-      values.push(type)
-      conditionArray.push(`AND sub.type = $${values.length}`)
-    }
-    console.log()
-    const conditionString = conditionArray.join(' ')
+    const { page } = req.query
+    const { conditionString, conditionValue } = res.locals
 
     const searchedInquiry = (
       await pgQuery(
         `SELECT
-        CEIL(inq.count::float / 10) AS "lastPage",
+        CEIL(inq_thread.count::float / 10) AS "lastPage",
         ARRAY_AGG(
           JSON_BUILD_OBJECT(
-            'needAnswer', inq.need_answer,
-            'id', inq.id, 
-            'title', inq.title,
-            'createdAt', TO_CHAR(inq.created_at, 'YYYY-MM-DD HH24:MI:SS'),
-            'updatedAt', TO_CHAR(inq.updated_at, 'YYYY-MM-DD HH24:MI:SS'),
-            'type', inq.type
-          ) ORDER BY inq.created_at DESC
-        ) AS thread
+            'needAnswer', inq_thread.need_answer,
+            'id', inq_thread.id, 
+            'title', inq_thread.title,
+            'createdAt', TO_CHAR(inq_thread.created_at, 'YYYY-MM-DD HH24:MI:SS'),
+            'updatedAt', TO_CHAR(inq_thread.updated_at, 'YYYY-MM-DD HH24:MI:SS'),
+            'type', inq_thread.type
+          ) ORDER BY inq_thread.created_at DESC
+        ) AS list
       FROM (
-          SELECT inq_na.need_answer, sub.id, sub.title, sub.created_at, sub.updated_at, sub.type,
-          COUNT(*) OVER() AS count
-          FROM 
-            (SELECT id, author_id, title, created_at, updated_at, thread_id, type,
-            ROW_NUMBER() OVER (PARTITION BY thread_id ORDER BY created_at ASC) AS rn
-            FROM kkujjang.inquiry) sub
-          LEFT JOIN kkujjang.inquiry_need_answer inq_na ON inq_na.thread_id = sub.thread_id
-          WHERE sub.rn = 1 ${conditionString}
-          ORDER BY sub.created_at DESC
+          SELECT *, COUNT(*) OVER() AS count
+          FROM kkujjang.inquiry_thread
+          WHERE 1 = 1 ${conditionString}
+          ORDER BY created_at DESC
           OFFSET ${(Number(page) - 1) * 10} LIMIT 10
-      ) inq
-      GROUP BY inq.count`,
-        values,
+      ) inq_thread
+      GROUP BY inq_thread.count`,
+        conditionValue,
       )
     ).rows
 
@@ -80,44 +63,36 @@ inquiryRouter.get(
   '/list',
   requireSignin,
   validatePageNumber,
+  validateInquiryGetByList,
   async (req, res) => {
-    const session = res.locals.session
     const { page } = req.query
-
-    let conditionString = ''
-    let values = []
-    if (session.authorityLevel !== process.env.ADMIN_AUTHORITY) {
-      conditionString = 'AND sub.author_id = $1'
-      values.push(session.userId)
-    }
+    const { conditionString = '', conditionValue = [] } = res.locals
 
     const listedInquiry = (
       await pgQuery(
         `SELECT
-        CEIL(inq.count::float / 10) AS "lastPage",
+        CEIL(inq_thread.count::float / 10) AS "lastPage",
         ARRAY_AGG(
           JSON_BUILD_OBJECT(
-            'needAnswer', inq_na.need_answer,
-            'id', inq.id, 
-            'title', inq.title,
-            'createdAt', TO_CHAR(inq.created_at, 'YYYY-MM-DD HH24:MI:SS'),
-            'updatedAt', TO_CHAR(inq.updated_at, 'YYYY-MM-DD HH24:MI:SS'),
-            'type', inq.type
-          ) ORDER BY inq.created_at DESC
-        ) AS threads
-      FROM (
-          SELECT *, COUNT(*) OVER() AS count
-          FROM 
-            (SELECT id, author_id, title, created_at, updated_at, thread_id, type,
-            ROW_NUMBER() OVER (PARTITION BY thread_id ORDER BY created_at ASC) AS rn
-            FROM kkujjang.inquiry) sub
-          WHERE sub.rn = 1 ${conditionString}
-          ORDER BY sub.created_at DESC
-          OFFSET ${(Number(page) - 1) * 10} LIMIT 10
-      ) inq
-      LEFT JOIN kkujjang.inquiry_need_answer inq_na ON inq.thread_id = inq_na.thread_id
-      GROUP BY inq.count`,
-        values,
+            'needAnswer', inq_thread.need_answer,
+            'id', inq_thread.id, 
+            'title', inq_thread.title,
+            'createdAt', TO_CHAR(inq_thread.created_at, 'YYYY-MM-DD HH24:MI:SS'),
+            'updatedAt', TO_CHAR(inq_thread.updated_at, 'YYYY-MM-DD HH24:MI:SS'),
+            'type', inq_thread.type
+          ) ORDER BY inq_thread.created_at DESC
+        ) AS list
+        
+        FROM (
+            SELECT *, COUNT(*) OVER() AS count
+            FROM kkujjang.inquiry_thread
+            ${conditionString}
+            ORDER BY created_at DESC
+            OFFSET ${(Number(page) - 1) * 10} LIMIT 10
+        ) inq_thread
+
+        GROUP BY inq_thread.count`,
+        conditionValue,
       )
     ).rows
 
@@ -131,88 +106,72 @@ inquiryRouter.get(
 inquiryRouter.get(
   '/:inquiryId',
   requireSignin,
-  validateInquiryPathIndex,
+  validatePageNumber,
+  validateInquiryGetByPathIndex,
   async (req, res) => {
-    const session = res.locals.session
+    const { page } = req.query
     const { inquiryId } = req.params
-    const values = [inquiryId, process.env.ADMIN_AUTHORITY]
+    const { conditionString = '', conditionValue = [] } = res.locals
 
-    let conditionString = ''
-    if (
-      Number(session.authorityLevel) !== Number(process.env.ADMIN_AUTHORITY)
-    ) {
-      conditionString = 'AND author_id = $3'
-      values.push(session.userId)
-    }
-
-    const mainThread = (
+    const inquiryByInquiryId = (
       await pgQuery(
         `SELECT
-      inq_na.need_answer AS "needAnswer",
-      CASE
-        WHEN usr.authority_level = $2 THEN true
-        ELSE false
-      END AS "isAnswer",
-      inq.thread_id AS "threadId",
-      inq.author_id AS "authorId", usr.nickname, inq.title AS "threadTitle", inq.content AS "threadContent",
-      TO_CHAR(inq.updated_at, 'YYYY-MM-DD HH24:MI:SS') as "updatedAt",
-      ARRAY_AGG(inq_f.key ORDER BY inq_f.file_order ASC) AS "threadFiles" 
-      FROM kkujjang.inquiry inq
-      LEFT JOIN kkujjang.inquiry_need_answer inq_na ON inq_na.thread_id = inq.thread_id
-      LEFT JOIN kkujjang.inquiry_file inq_f ON inq_f.inquiry_id = inq.id
-      LEFT JOIN kkujjang.user usr ON usr.id = inq.author_id 
-      WHERE inq.thread_id = (
-        SELECT thread_id
-        FROM kkujjang.inquiry
-        WHERE id = $1 ${conditionString}
-      )
-      GROUP BY inq.thread_id, usr.authority_level, usr.nickname, inq_na.need_answer, inq.author_id, inq.title, inq.content, inq.created_at, inq.updated_at
-      ORDER BY inq.created_at ASC
-      LIMIT 1`,
-        values,
+        inq_thread.need_answer AS "needAnswer",
+        inq_thread.thread_id AS "threadId",
+        inq_thread.author_id AS "authorId",
+        inq_thread.type AS "threadType",
+        inq_thread.title AS "threadTitle",
+        usr.nickname AS "nickname",
+        TO_CHAR(inq_thread.updated_at, 'YYYY-MM-DD HH24:MI:SS') as "updatedAt",
+        CEIL(inq_article.count::float / 10) AS "lastPage",
+
+        ARRAY_AGG(
+          JSON_BUILD_OBJECT(
+            'isAnswer', CASE WHEN usr.authority_level = $2 THEN true ELSE false END,
+            'authorId', inq_article.author_id,
+            'content', inq_article.content,
+            'file', inq_files.files
+          ) ORDER BY inq_article.created_at ASC
+        ) AS list
+
+        FROM (
+          SELECT *, COUNT(*) OVER() AS count
+          FROM kkujjang.inquiry_article 
+          ORDER BY created_at DESC
+          OFFSET(${(Number(page) - 1) * 10}) LIMIT 10
+        ) inq_article
+
+        LEFT JOIN kkujjang.inquiry_thread inq_thread ON inq_thread.thread_id = inq_article.thread_id
+        LEFT JOIN kkujjang.user usr ON usr.id = inq_article.author_id 
+        LEFT JOIN (
+          SELECT inquiry_id, ARRAY_AGG(key ORDER BY file_order ASC) AS files
+          FROM kkujjang.inquiry_file
+          GROUP BY inquiry_id
+        ) inq_files ON inq_files.inquiry_id = inq_article.id  
+        
+        WHERE inq_article.thread_id = (
+          SELECT thread_id
+          FROM kkujjang.inquiry_article
+          WHERE id = $1 ${conditionString}
+        )
+
+        GROUP BY 
+            inq_thread.need_answer, inq_thread.thread_id, inq_thread.author_id, 
+            inq_thread.type, inq_thread.title, usr.nickname, 
+            inq_thread.updated_at, inq_article.count`,
+        [inquiryId, process.env.ADMIN_AUTHORITY, ...conditionValue],
       )
     ).rows
 
-    if (mainThread.length === 0) {
+    if (inquiryByInquiryId.length === 0) {
       throw {
         statusCode: 404,
         message: '존재하지 않는 페이지입니다',
       }
     }
 
-    delete mainThread[0]['isAnswer']
-
-    const subThread = (
-      await pgQuery(
-        `SELECT
-      CASE
-        WHEN usr.authority_level = $2 THEN true
-        ELSE false
-      END AS "isAnswer",
-      inq.author_id AS "authorId", inq.title, inq.content,
-      TO_CHAR(inq.updated_at, 'YYYY-MM-DD HH24:MI:SS') as "updatedAt",
-      ARRAY_AGG(inq_f.key ORDER BY inq_f.file_order ASC) AS files
-      FROM kkujjang.inquiry inq
-      LEFT JOIN kkujjang.inquiry_need_answer inq_na ON inq_na.thread_id = inq.thread_id
-      LEFT JOIN kkujjang.inquiry_file inq_f ON inq_f.inquiry_id = inq.id
-      LEFT JOIN kkujjang.user usr ON usr.id = inq.author_id 
-      WHERE inq.thread_id = (
-        SELECT thread_id
-        FROM kkujjang.inquiry
-        WHERE id = $1 ${conditionString}
-      )
-      GROUP BY inq.thread_id, usr.authority_level, usr.nickname, inq_na.need_answer, inq.author_id, inq.title, inq.content, inq.created_at, inq.updated_at
-      ORDER BY inq.created_at ASC
-      OFFSET 1`,
-        values,
-      )
-    ).rows
-
     res.json({
-      result: {
-        ...mainThread[0],
-        thread: subThread,
-      },
+      result: inquiryByInquiryId,
     })
   },
 )
@@ -222,67 +181,35 @@ inquiryRouter.post(
   '/',
   requireSignin,
   validateInquiryupload,
-  validateInquiry,
+  validateInquiryPost,
   async (req, res) => {
     const session = res.locals.session
-    const needAnswer =
-      session.authorityLevel === process.env.ADMIN_AUTHORITY ? true : false
 
     const { title, content, id, type } = req.body
 
-    const inquiry = (
-      await pgQuery(
-        `WITH 
-      new_inquiry AS (
-        INSERT INTO kkujjang.inquiry (author_id, title, content, thread_id, type) VALUES ($1, $2, $3, $4, $5)
-        RETURNING id, thread_id
-      ),
-      inserted_inquiry_need_answer AS (
-        INSERT INTO kkujjang.inquiry_need_answer (thread_id, need_answer)
-        SELECT ni.thread_id, $6
-        FROM new_inquiry ni
-        ON CONFLICT (thread_id)
-        DO UPDATE SET need_answer = $6
-      )
-      SELECT id FROM new_inquiry`,
-        [session.userId, title, content, id, type, needAnswer],
-      )
-    ).rows[0].id
+    const {
+      fileInsertQuery = 'SELECT 1 FROM inserted_article LIMIT 1',
+      fileValue = [],
+      needAnswer,
+    } = res.locals
 
-    if (req.files.length === 0) {
-      res.json({
-        result: 'success  ',
-      })
-    }
-
-    const fileQueryPrefix = `INSERT INTO kkujjang.inquiry_file (inquiry_id, file_order, key)`
-
-    const fileAttributes = req.files.map((file, index) => ({
-      order: index + 1,
-      url: file,
-      argOrder: (index + 1) * 2,
-    }))
-
-    const fileValues = [
-      inquiry,
-      ...fileAttributes.reduce(
-        (prev, fileAttribute) => [
-          ...prev,
-          fileAttribute.order,
-          fileAttribute.url,
-        ],
-        [],
-      ),
-    ]
-
-    const valueStringArray = fileAttributes.map(
-      (fileAttribute) =>
-        `($1, $${fileAttribute.argOrder}, $${fileAttribute.argOrder + 1})`,
+    await pgQuery(
+      `WITH
+        inserted_article AS (
+          INSERT INTO kkujjang.inquiry_article (author_id, thread_id, content)
+          VALUES ($1, $2, $3)
+          RETURNING author_id, id, thread_id
+        ),
+        inserted_thread AS (
+          INSERT INTO kkujjang.inquiry_thread (need_answer, author_id, inquiry_id, thread_id, type, title)  
+          SELECT $4, author_id, id, thread_id, $5, $6
+          FROM inserted_article
+          ON CONFLICT (thread_id)
+          DO UPDATE SET need_answer = $4, updated_at = CURRENT_TIMESTAMP
+        )
+        ${fileInsertQuery}`,
+      [session.userId, id, content, needAnswer, type, title, ...fileValue],
     )
-    const valueString = valueStringArray.join(', ')
-    const fileQuery = [fileQueryPrefix, 'VALUES', valueString].join(' ')
-
-    await pgQuery(fileQuery, fileValues)
 
     res.json({ result: 'success' })
   },
