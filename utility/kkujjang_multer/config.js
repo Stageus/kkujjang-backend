593de414864bdbad4d5bd1a2de07edb971b3bae5
굴소스 <139232765+GulSauce@users.noreply.github.com>
2024-01-14 14:49:42 +0900

    Refactor: remain only s3 upload and its validations in custom multer script
    * Refactor: multer
    
    * Feat: multer
    
    * Refactor: multer option input logic
    
    * Rename: move utility functions
    
    * Docs: remove filecount router in  /test

---
+++ utility/kkujjang_multer/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  // 최대 파일명(확장자 포함) 길이
+  fileNameLength: 200,
+  // 필드 이름의 최대 Byte
+  fieldNameSize: 100,
+  // text value의 총합 최대 Byte
+  fieldSize: 1024 * 1024,
+  // text filed의 총합 개수
+  fields: 50,
+}
