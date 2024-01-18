cdd3ec0f564b4d2c102b04c17585dead2e2ac984
GulSauce <pine7420@naver.com>
2024-01-18 20:57:15 +0900

    Feat: try join game room more safe

--- socket/game-room.js
+++ socket/game-room.js
@@ -2,6 +2,20 @@ import * as uuid from 'uuid'
 
 export const gameRooms = {}
 export const gameRoomPasswords = {}
+export const clientRoomId = {}
+
+const nickname = [
+  '감자',
+  '다래',
+  '슬기',
+  '머루쉐',
+  '손인욱',
+  '김찬호',
+  '김스테이지어스',
+  '이스테이지어스',
+  '박스테이지어스',
+  '최스테이지어스',
+]
 
 const addChatEventLinstener = (gameRoomNamespace, gameRoomId, socket) => {
   socket.on('chat', (msg) => {
@@ -28,45 +42,64 @@ const emitRefreshGameRoom = (lobbyNamespace, gameRoomId) => {
   )
 }
 
-const nickname = [
-  '감자',
-  '머루',
-  '다래',
-  '슬기',
-  '김스테이지어스',
-  '이스테이지어스',
-  '박스테이지어스',
-  '최스테이지어스',
-]
+const leaveGameRoom = (
+  lobbyNamespace,
+  gameRoomNamespace,
+  userInfo,
+  leftGameRoomId,
+) => {
+  const curRoomMeberCount = --gameRooms[leftGameRoomId].memberCount
+  const curRooMembers = gameRooms[leftGameRoomId].members
+
+  const index = curRooMembers.indexOf(userInfo)
+  if (index > -1) {
+    curRooMembers.splice(index, 1)
+  }
+  gameRooms[leftGameRoomId].members = curRooMembers
+
+  if (curRoomMeberCount === 0) {
+    lobbyNamespace.emit('remove game room', leftGameRoomId)
+    delete gameRooms[leftGameRoomId]
+    delete gameRoomPasswords[leftGameRoomId]
+  } else {
+    emitRefreshGameRoom(lobbyNamespace, leftGameRoomId)
+
+    gameRoomNamespace
+      .to(leftGameRoomId)
+      .emit('remove game room member', userInfo.id)
+  }
+}
 
 export const createGameRoomSocket = (gameRoomNamespace, lobbyNamespace) => {
   gameRoomNamespace.on('connect', (socket) => {
     const randomIndex = Math.floor(Math.random() * 8)
-    // 최초 연결시 유저 정보 저장
-    if (socket.userInfo === undefined) {
-      socket.userInfo = {}
-      socket.userInfo.id = randomIndex
-      socket.userInfo.nickname = nickname[randomIndex]
-      socket.userInfo.level = 1
-    }
+
+    socket.userInfo = {}
+    socket.userInfo.id = randomIndex
+    socket.userInfo.nickname = nickname[randomIndex]
+    socket.userInfo.level = 1
 
     // 방 만들기 이벤트리스너 등록
-    socket.on('create game room', (roomInfo) => {
+    socket.on('create game room', (newRoomInfo) => {
       const gameRoomId = uuid.v4()
-      const { title, password, memberLimit, roundCount } = roomInfo
+      const { title, password, memberLimit, roundCount, roundTimeLimit } =
+        newRoomInfo
 
       const isPasswordRoom = password !== '' ? true : false
       gameRoomPasswords[gameRoomId] = password
 
       const gameRoomInfo = (gameRooms[gameRoomId] = {
         isPasswordRoom,
+        isInGame: false,
         title,
         memberCount: 1,
         memberLimit,
         roundCount,
+        roundTimeLimit,
         members: [socket.userInfo],
       })
 
+      clientRoomId[socket.id] = gameRoomId
       socket.join(gameRoomId)
 
       emitDrawGameRoom(socket, gameRoomInfo)
@@ -83,15 +116,39 @@ export const createGameRoomSocket = (gameRoomNamespace, lobbyNamespace) => {
     })
 
     // 방 참가하기 이벤트리스너 등록
-    socket.on('join game room', (gameRoomId) => {
+    socket.on('try join game room', (joinTicket) => {
+      const { gameRoomId, password } = joinTicket
+      const { isPasswordRoom, isInGame, memberCount, memberLimit } =
+        gameRooms[gameRoomId]
+
+      if (memberLimit <= memberCount) {
+        socket.emit('fail join game room', '이미 풀방입니다')
+        socket.disconnect(true)
+        return
+      }
+      if (isInGame) {
+        socket.emit('fail join game room', '게임중인 방입니다')
+        socket.disconnect(true)
+        return
+      }
+
+      if (isPasswordRoom && gameRoomPasswords[gameRoomId] !== password) {
+        socket.emit('fail join game room', '비밀번호가 일치하지 않습니다')
+        socket.disconnect(true)
+        return
+      }
+
+      // 여기서부턴 접속 성공
+
+      socket.join(gameRoomId)
+      clientRoomId[socket.id] = gameRoomId
+      gameRooms[gameRoomId].memberCount++
+      gameRooms[gameRoomId].members.push(socket.userInfo)
+
       gameRoomNamespace
         .to(gameRoomId)
         .emit('add game room member', JSON.stringify(socket.userInfo))
 
-      socket.join(gameRoomId)
-      gameRooms[gameRoomId].memberCount++
-
-      gameRooms[gameRoomId].members.push(socket.userInfo)
       const gameRoomInfo = gameRooms[gameRoomId]
 
       addChatEventLinstener(gameRoomNamespace, gameRoomId, socket)
@@ -99,33 +156,18 @@ export const createGameRoomSocket = (gameRoomNamespace, lobbyNamespace) => {
       emitRefreshGameRoom(lobbyNamespace, gameRoomId)
     })
 
-    // 방 나가기 이벤트리스너 등록
-    socket.on('leave game room', () => {
-      for (const gameRoomId of socket.rooms) {
-        if (gameRoomId === socket.id) {
-          continue
-        }
-
-        const curRoomMeberCount = --gameRooms[gameRoomId].memberCount
-        const curRooMembers = gameRooms[gameRoomId].members
-
-        const index = curRooMembers.indexOf(socket.userInfo)
-        if (index > -1) {
-          curRooMembers.splice(index, 1)
-        }
-        gameRooms[gameRoomId].members = curRooMembers
-
-        if (curRoomMeberCount === 0) {
-          lobbyNamespace.emit('remove game room', gameRoomId)
-          delete gameRooms[gameRoomId]
-          delete gameRoomPasswords[gameRoomId]
-        } else {
-          emitRefreshGameRoom(lobbyNamespace, gameRoomId)
-          gameRoomNamespace
-            .to(gameRoomId)
-            .emit('remove game room member', socket.userInfo.id)
-        }
+    socket.on('disconnect', () => {
+      const leftGameRoomId = clientRoomId[socket.id]
+      if (leftGameRoomId === undefined) {
+        return
       }
+      delete clientRoomId[socket.id]
+      leaveGameRoom(
+        lobbyNamespace,
+        gameRoomNamespace,
+        socket.userInfo,
+        leftGameRoomId,
+      )
     })
   })
 }
