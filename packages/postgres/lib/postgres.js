0c14e271153b26075717061b6c29673d11bdaf6a
Godbell <12191579@inha.edu>
2024-02-03 01:54:02 +0900

    Chore: separate postgres into a package

---
+++ packages/postgres/lib/postgres.js
@@ -0,0 +1,49 @@
+// @ts-check
+
+import { configDotenv } from 'dotenv'
+import pg from 'pg'
+
+configDotenv()
+
+const dbConfig = {
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database:
+    process.env.NODE_ENV === 'production'
+      ? process.env.POSTGRES_NAME
+      : process.env.POSTGRES_TEST_NAME,
+}
+
+const pgPool = new pg.Pool(dbConfig)
+
+pgPool.on('error', (err, client) => {
+  console.error('Unexpected Error on PG Client.', err)
+})
+
+console.log(
+  `Created PG Pool to ${dbConfig.host}:${dbConfig.port} with user ${dbConfig.user}`,
+)
+
+/**
+ * @param {string} query
+ * @param {*[]} parameters
+ * @returns {Promise<pg.QueryResult<any>>}
+ */
+export const pgQuery = async (query, parameters = undefined) => {
+  const connection = await pgPool.connect()
+  let result = null
+
+  try {
+    result = await connection.query(query, parameters)
+  } catch (e) {
+    throw e
+  } finally {
+    connection.release()
+  }
+
+  return result
+}
+
+export default pgQuery
