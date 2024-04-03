21f1c2b11751baa251fb162fb5275aa64ac0c204
GulSauce <pine7420@naver.com>
2024-04-03 21:34:20 +0900

    Feat: erros in api & logger

--- packages/kkujjang-api-server/src/middleware/chat.js
+++ packages/kkujjang-api-server/src/middleware/chat.js
@@ -1,13 +1,22 @@
 import * as validation from 'kkujjang-validation'
 
 export const validateChatSearchQuery = (req, res, next) => {
-  const { userId, dateStart, dateEnd } = req.query
+  const { userId, roomId, dateStart, dateEnd } = req.query
 
   userId &&
     validation.check(
       userId,
       'userId',
       validation.checkParsedNumberInRange(0, Infinity),
+    )
+  roomId &&
+    validation.check(
+      roomId,
+      'roomId',
+      validation.checkExist(),
+      validation.checkRegExp(
+        /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/,
+      ),
     )
   dateStart &&
     validation.check(
